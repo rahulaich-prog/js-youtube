a3dd52d0fdb313073ba1d1939701a3e06e33c6a0
rahulaich-prog <aichrahul8906@gmail.com>
2025-02-05 16:25:15 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144554
+let accountEmail = "aichrahul8906"
+var accountPass = "12345"
+accountcity = "jaipur"
+
+// accountId = 2
+/*
+prefer not to use var
+because of issues in block scope and funtonal scope
+*/
+
+accountEmail = "hc@hac.com"
+accountPassword = "212121"
+accountcity = "cuttack"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity])
